b128b7d0455cda807d147d00f12ae6a7ae3631ed
Jacob W <jtwalsh7@gmail.com>
2024-09-14 13:28:45 -0400

    FIX : attempting to format for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-  base: './',
+  base: "https://menacinglamprey.github.io/CocktailSubmission/",
   plugins: [react()],
   server :{
     port :3000
