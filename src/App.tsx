65ad00dea7c320d6731cd8ba5d3a7599c98fe360
Jacob W <jtwalsh7@gmail.com>
2024-09-14 01:30:11 -0400

    FIX : attempting to format for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "/CocktailSubmission",
       element: <Box>
         <NavBar/>
         <DrinkForm />
