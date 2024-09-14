d634427bbe768f4898fa3a24925f92b29eae4c68
Jacob W <jtwalsh7@gmail.com>
2024-09-14 01:21:36 -0400

    Updates

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig(function (_a) {
+    var mode = _a.mode;
+    return {
+        plugins: [react()],
+        server: {
+            port: 3000
+        },
+        define: {
+            'process.env': __assign({}, process.env)
+        }
+    };
+});
